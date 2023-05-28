5669abd6707132baaed700dc8623ab6cba84149a
Rest <vladafanasevrest@gmail.com>
2023-05-28 20:19:23 +0500

    [MS-17] Swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  ConfigModule.forRoot({
+    envFilePath: `.env.development`,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('MouseAPI')
+    .setDescription('MouseAPI application')
+    .setVersion('0.0.1')
+    .addBearerAuth()
+    .build();
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: [
+      "http://localhost:3000"
+    ],
+    allowedHeaders: [
+      'Accept',
+      'Content-Type',
+      'Authorization',
+    ],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true
+  });
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
+  await app.listen(8000);
 }
 bootstrap();
